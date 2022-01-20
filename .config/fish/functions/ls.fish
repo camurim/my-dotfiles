4a76d083b3d7d8de2355c2c7a46ae39ccfc0835c
Carlos Amorim <caamorimcoelho@gmail.com>
2022-01-19 23:37:52 -0300

    Updates

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ls --wraps='exa -lmr' --wraps='ls -l --sort=mod -r --icons' --wraps='exa -l --sort=mod -r --icons' --description 'alias ls exa -l --sort=mod -r --icons'
-  exa -l --sort=mod -r --icons $argv;
+function ls --wraps='exa -l --sort=mod -r --long  --header --icons' --wraps='exa -l --sort=mod -r --header --icons' --wraps='exa -l --sort=mod -r --long --header --icons' --wraps='exa -lh --sort=mod -r --long --header --icons' --description 'alias ls exa -lh --sort=mod -r --long --header --icons'
+  exa -lh --sort=mod -r --long --header --icons $argv;
 end
