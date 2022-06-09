e381eb55cf115b94db9d2609b9f2ca83b960476a
Carlos Amorim <caamorimcoelho@gmail.com>
2022-06-08 22:26:37 -0300

    update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,37 @@
+#░█▀▀░█▀█░█░░░█▀█░█▀▄░█▀▀
+#░█░░░█░█░█░░░█░█░█▀▄░▀▀█
+#░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+
+set -U COLOR_RESET "\033[0m"
+
+set -U COLOR_K "\033[0;30m"    # black
+set -U COLOR_R "\033[0;31m"    # red
+set -U COLOR_G "\033[0;32m"    # green
+set -U COLOR_Y "\033[0;33m"    # yellow
+set -U COLOR_B "\033[0;34m"    # blue
+set -U COLOR_M "\033[0;35m"    # magenta
+set -U COLOR_C "\033[0;36m"    # cyan
+set -U COLOR_W "\033[0;37m"    # white
+
+# empahsized (bolded) colors
+set -U EM_K "\033[1;30m"
+set -U EM_R "\033[1;31m"
+set -U EM_G "\033[1;32m"
+set -U EM_Y "\033[1;33m"
+set -U EM_B "\033[1;34m"
+set -U EM_M "\033[1;35m"
+set -U EM_C "\033[1;36m"
+set -U EM_W "\033[1;37m"
+
+# background colors
+set -U BG_K "\033[40m"
+set -U BG_R "\033[41m"
+set -U BG_G "\033[42m"
+set -U BG_Y "\033[43m"
+set -U BG_B "\033[44m"
+set -U BG_M "\033[45m"
+set -U BG_C "\033[46m"
+set -U BG_W "\033[47m"
 
 #░█▀█░█▀█░▀█▀░█░█
 #░█▀▀░█▀█░░█░░█▀█
@@ -233,6 +267,15 @@ function ftpd --argument stat -d "Controls vsftpd service"
             case '*'
                  echo "Incorrect parameter!"
          end
+end
+
+# Download and cut video
+function dwAndCutVid --argument url --argument start --argument end --argument output -d "Download and cut video"
+         if test (count $argv) -lt 4 -o "$argv[1]" = "--help"
+            printf "%b" "$EM_RED\e0USAGE: dwAndCutVid <URL> <START> <END> <OUTPUT_FILE>$COLOR_REST"
+            return 1
+         end
+         yt-dlp -f mp4 "$url" -o - | ffmpeg -f mp4 -i - -ss "$start" -to "$end" -c:v copy "$output"
 end
 
 #░█▀█░█░░░▀█▀░█▀█░█▀▀░█▀▀░█▀▀
