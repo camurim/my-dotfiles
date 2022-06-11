68ab21077eae8880436dc69142f223c3cbde6488
Carlos Amorim <caamorimcoelho@gmail.com>
2022-06-11 20:48:59 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -394,7 +394,7 @@ alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
 alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
 
 # bare git repo alias for dotfiles
-alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+alias config="/usr/bin/git --git-dir='$HOME/src/my-dotfiles/.git' --work-tree=$HOME"
 
 # termbin
 alias tb="nc termbin.com 9999"
