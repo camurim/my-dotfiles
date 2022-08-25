1f36cda7957e76c3b548243be23197c38dad1dd1
Carlos Amorim <caamorimcoelho@gmail.com>
2022-08-25 17:45:03 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -505,6 +505,9 @@ alias tb="nc termbin.com 9999"
 # the terminal rickroll
 alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
 
+# the terminal bad apple
+alias badapple='CACA_DRIVER=ncurses mpv -vo=caca https://youtu.be/9lNZ_Rnr7Jc 2>/dev/null'
+
 # Unlock LBRY tips
 alias tips="lbrynet txo spend --type=support --is_not_my_input --blocking"
 
