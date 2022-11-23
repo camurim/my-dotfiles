600fb8a12688116d6088d7c8f59d54283733a2cb
Carlos Amorim <caamorimcoelho@gmail.com>
2022-11-23 16:43:12 -0300

    update OMF theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -830,50 +830,15 @@ alias t='todo.sh'
 # View WebCam stream
 alias webcam='mpv /dev/video1'
 
+# Twitter CLI client
+alias tw='rainbowstream'
+
 #░▀█▀░█▀▀░█▀▄░█▄█░▀█▀░█▀█░█▀█░█░░░░░█▀█░█▀▄░█▀█░█▄█░█▀█░▀█▀
 #░░█░░█▀▀░█▀▄░█░█░░█░░█░█░█▀█░█░░░░░█▀▀░█▀▄░█░█░█░█░█▀▀░░█░
 #░░▀░░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░░░▀░░░▀░▀░▀▀▀░▀░▀░▀░░░░▀░
 
-set -g theme_display_git yes
-set -g theme_display_git_dirty yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_display_git_dirty_verbose yes
-set -g theme_display_git_stashed_verbose yes
-set -g theme_display_git_default_branch yes
-set -g theme_git_default_branches master main
-set -g theme_git_worktree_support yes
-set -g theme_use_abbreviated_branch_name yes
-set -g theme_display_vagrant yes
-set -g theme_display_docker_machine no
-set -g theme_display_k8s_context yes
-set -g theme_display_hg yes
-set -g theme_display_virtualenv yes
-set -g theme_display_nix no
-set -g theme_display_ruby no
-set -g theme_display_node yes
-set -g theme_display_user ssh
-set -g theme_display_hostname ssh
-set -g theme_display_vi yes
-set -g theme_display_date yes
-set -g theme_display_cmd_duration yes
-set -g theme_title_display_process yes
-set -g theme_title_display_path no
-set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path no
-set -g theme_date_format "+%a %H:%M"
-set -g theme_date_timezone America/Fortaleza
-set -g theme_avoid_ambiguous_glyphs yes
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
-set -g theme_display_jobs_verbose yes
-#set -g default_user your_normal_user
-set -g theme_color_scheme dracula
-set -g fish_prompt_pwd_dir_length 0
-set -g theme_project_dir_length 1
-set -g theme_newline_cursor no
-#set -g theme_newline_prompt '$ '
+# Oh My Fish! Agnoster Theme
+set -g fish_git_prompt_untracked_files yes
 
 #starship init fish | source
 # Powerline
