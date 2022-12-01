f176a37448879ddadf321e1c93651b4f3f46ac53
Carlos Amorim <caamorimcoelho@gmail.com>
2022-12-01 18:35:57 -0300

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -72,9 +72,9 @@ set -x TODO_FILE $TODO_DIR/todo.txt
 set -x DONE_FILE $TODO_DIR/done.txt
 
 #set fish_greeting                                 # Supresses fish's intro message
-set TERM "xterm-256color"                         # Sets the terminal type
-set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
-set VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+set -x TERM "xterm-256color"                         # Sets the terminal type
+set -x EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
+set -x VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
 
 ### SET MANPAGER
 ### Uncomment only one of these!
