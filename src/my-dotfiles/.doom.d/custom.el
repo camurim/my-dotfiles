33409edbfeb3afb04fceb4eb8a7e2d9d58782e0e
Carlos Amorim <caamorimcoelho@gmail.com>
2022-12-06 06:39:12 -0300

    update Doom Emacs config files

---
+++ src/my-dotfiles/.doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(fish-mode wakatime-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(global-set-key [f7] 'neotree-change-root)
