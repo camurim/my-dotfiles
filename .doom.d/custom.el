748a2cdd961ca6556261ab70cf0dc13a0df575df
Carlos Amorim <caamorimcoelho@gmail.com>
2023-04-03 19:22:06 -0300

    update Doom Emacs config

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -23,3 +23,68 @@
   (interactive)
   (shell-command (concat "python " buffer-file-name)))
 (global-set-key (kbd "<f9>") 'run-buffer)
+
+;; --------------------------------------------------------
+
+(defun malb/indent-fold-or-complete (&optional arg)
+  (interactive "P")
+  (cond
+   ;; if a region is active, indent
+   ((use-region-p)
+    (indent-region (region-beginning)
+                   (region-end)))
+
+   ;; always fold if we're looking at an org heading
+   ((and (eq major-mode 'org-mode)
+         (or (org-at-drawer-p)
+             (org-at-heading-p)))
+    (save-excursion (org-cycle)))
+
+   ;; always use table actions in org tables
+   ((and (eq major-mode 'org-mode) (org-table-p))
+    (save-excursion (call-interactively 'orgtbl-tab)))
+
+   ;; leave the math env
+   ((and (or (and (eq major-mode 'org-mode) (bound-and-true-p org-cdlatex-mode))
+             (and (eq major-mode 'latex-mode) (bound-and-true-p cdlatex-mode)))
+         (texmathp)
+         ;; (or (looking-at "\\\\)")
+         ;;     (looking-at "\\]")
+         ;;     (looking-at "}"))
+         )
+    (cdlatex-tab))
+
+   ;; arg triggers cdlatex
+   ((or
+     (and (eq major-mode 'org-mode) (bound-and-true-p org-cdlatex-mode) arg)
+     (and (eq major-mode 'latex-mode) (bound-and-true-p cdlatex-mode) arg))
+    (cdlatex-tab))
+
+   ;; if the next char is space or eol, but prev char not whitespace
+   ((and (not (active-minibuffer-window))
+         (or (looking-at " ")
+             (looking-at "$"))
+         (looking-back "[^[:space:]]" nil)
+         (not (and (eq major-mode 'org-mode) (looking-back "<[a-z]+" nil)))
+         (not (looking-back "^" nil)))
+    (company-complete-common))
+
+   ;; no whitespace anywhere
+   ((and (not (active-minibuffer-window))
+         (looking-at "[^[:space:]]")
+         (looking-back "[^[:space:]]" nil)
+         (not (looking-back "^" nil)))
+    (cond ((bound-and-true-p cdlatex-mode) (cdlatex-tab))
+          ((bound-and-true-p hs-minor-mode) (save-excursion (end-of-line) (hs-toggle-hiding)))
+          ((bound-and-true-p outline-minor-mode) (save-excursion (outline-cycle)))))
+
+   ;; by default just call whatever was bound
+   (t
+    (let ((fn (or (lookup-key (current-local-map) (kbd "TAB"))
+                  'indent-for-tab-command)))
+      (if (not (called-interactively-p 'any))
+          (fn arg)
+        (setq this-command fn)
+        (call-interactively fn))))))
+
+(bind-key "<tab>" #'malb/indent-fold-or-complete)
